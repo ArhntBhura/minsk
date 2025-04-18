641ce643875bd2643dd7d55f4f8a9f671feb8f6a
ArhntBhura <arihantbhura06@gmail.com>
2025-04-18 15:57:50 +0530

    Added testing conditions and added SyntaxTree.ParseTokens() method for exposing each token, easy for testing

--- Minsk.Tests/obj/Debug/net9.0/Minsk.Tests.AssemblyInfo.cs
+++ Minsk.Tests/obj/Debug/net9.0/Minsk.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minsk.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8f359c4238717726baeb0bac5d0523f6305d00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d69b4459d608332b6be24f83d3f2012f8e0ad0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minsk.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minsk.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
