c42b73323f249f2d1c3e9c110015a58aa46f7586
ArhntBhura <arihantbhura06@gmail.com>
2025-04-22 01:37:40 +0530

    Corrected the SourceText and SyntaxTree errors

--- mc/obj/Debug/net9.0/mc.AssemblyInfo.cs
+++ mc/obj/Debug/net9.0/mc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f504314d0a1c1437683f050217f29de7d9b15bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cad33f6ad2879ae709f1c54a2254eba50fea32")]
 [assembly: System.Reflection.AssemblyProductAttribute("mc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
